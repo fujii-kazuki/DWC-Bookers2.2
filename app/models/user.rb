22965e4c7f3ddc701e26e315b5822d7fa3484821
fujii-kazuki <142955783+fujii-kazuki@users.noreply.github.com>
2023-09-11 09:12:46 +0000

    [Add] RelationshipモデルとUserモデルを関連付け

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,12 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+
+  has_many :followers, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy
+  has_many :followeds, class_name: 'Relationship', foreign_key: 'followed_id', dependent: :destroy
+  has_many :followings_user, through: :followers, source: :follower
+  has_many :followeds_user, through: :followeds, source: :followed
+
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true

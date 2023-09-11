ce37f5392ba6b7ec192404b9160e21772e3ea972
fujii-kazuki <142955783+fujii-kazuki@users.noreply.github.com>
2023-09-11 09:34:46 +0000

    [Update] relationshipsコントローラーのcreate, destroyアクションの内容を追記

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,9 +1,17 @@
 class RelationshipsController < ApplicationController
   def create
+    unless Relationship.exists?(follower_id: current_user.id, followed_id: params[:user_id])
+      relationship = Relationship.new(follower_id: current_user.id, followed_id: params[:user_id])
+      relationship.save
+    end
     redirect_back(fallback_location: root_path)
   end
 
   def destroy
+    if Relationship.exists?(follower_id: current_user.id, followed_id: params[:user_id])
+      relationship = Relationship.find_by(follower_id: current_user.id, followed_id: params[:user_id])
+      relationship.destroy
+    end
     redirect_back(fallback_location: root_path)
   end
 end
